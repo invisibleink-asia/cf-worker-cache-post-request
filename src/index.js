5d3aa1df34a32022076d236bd907d3f323e70cbd
Jirayu <me@jirayu.in.th>
2025-06-04 18:15:24 +0700

    line break

--- src/index.js
+++ src/index.js
@@ -13,8 +13,10 @@ export default {
     async function sha256(message) {
       // encode as UTF-8
       const msgBuffer = await new TextEncoder().encode(message);
+
       // hash the message
       const hashBuffer = await crypto.subtle.digest("SHA-256", msgBuffer);
+
       // convert bytes to hex string
       return [...new Uint8Array(hashBuffer)]
         .map((b) => b.toString(16).padStart(2, "0"))
@@ -23,11 +25,14 @@ export default {
     try {
       if (request.method.toUpperCase() === "POST") {
         const body = await request.clone().text();
+
         // Hash the request body to use it as a part of the cache key
         const hash = await sha256(body);
         const cacheUrl = new URL(request.url);
+
         // Store the URL in cache by prepending the body's hash
         cacheUrl.pathname = "/posts" + cacheUrl.pathname + hash;
+
         // Convert to a GET to be able to cache
         const cacheKey = new Request(cacheUrl.toString(), {
           headers: request.headers,
@@ -37,13 +42,16 @@ export default {
         const cache = caches.default;
         // Find the cache key in the cache
         let response = await cache.match(cacheKey);
+
         // Otherwise, fetch response to POST request from origin
         if (!response) {
           response = await fetch(request);
           ctx.waitUntil(cache.put(cacheKey, response.clone()));
         }
+
         return response;
       }
+
       return fetch(request);
     } catch (e) {
       return new Response("Error thrown " + e.message);
